c35aee4e67973d4a161e0ef1bd1deb1a1adab872
NikolinaKon <nk20190059@student.fon.bg.ac.rs>
2023-08-27 23:28:39 +0200

    klase i fotografije

---
+++ klase/koment.php
@@ -0,0 +1,43 @@
+<?php
+
+class Komentar {
+    private $naziv;
+    private $cena;
+    private $opis;
+    private $korId;
+  
+    public function getNaziv() {
+      return $this->naziv;
+    }
+  
+    public function setNaziv($naziv) {
+      $this->naziv = $naziv;
+    }
+  
+    public function getCena() {
+      return $this->cena;
+    }
+  
+    public function setCena($cena) {
+      $this->cena = $cena;
+    }
+  
+    public function getOpis() {
+      return $this->opis;
+    }
+  
+    public function setOpis($opis) {
+      $this->opis = $opis;
+    }
+  
+    public function getKorId() {
+      return $this->korId;
+    }
+  
+    public function setKorId($korId) {
+      $this->korId = $korId;
+    }
+  }
+  
+
+?>
